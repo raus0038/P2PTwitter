26cd542fed869562dda9306822f6edeb6905e44a
Rhys Austin <rhysbach95@gmail.com>
2015-10-09 20:22:55 +1100

    Decreased Server Response Time

--- src/P2PTServer.java
+++ src/P2PTServer.java
@@ -72,7 +72,7 @@ public class P2PTServer implements Runnable {
 				
 				
 				try {
-					Thread.sleep(1000);
+					Thread.sleep(100);
 				} catch (InterruptedException e) {
 					e.printStackTrace();
 				}
